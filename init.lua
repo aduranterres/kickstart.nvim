e97c5d50ec7625a4d701365156137a0bd204177b
Antonio Duran Terres <antonio@joomdle.com>
2025-10-20 00:26:03 +0200

    Plugins para PHP

--- init.lua
+++ init.lua
@@ -481,6 +481,56 @@ require('lazy').setup({
       },
     },
   },
+  -- PHP features
+  {
+    'ccaglak/phptools.nvim',
+    keys = {
+      { '<leader>lm', '<cmd>PhpTools Method<cr>' },
+      { '<leader>lc', '<cmd>PhpTools Class<cr>' },
+      { '<leader>ls', '<cmd>PhpTools Scripts<cr>' },
+      { '<leader>ln', '<cmd>PhpTools Namespace<cr>' },
+      { '<leader>lg', '<cmd>PhpTools GetSet<cr>' },
+      { '<leader>lf', '<cmd>PhpTools Create<cr>' },
+      { '<leader>ld', '<cmd>PhpTools DrupalAutoLoader<cr>' },
+      { mode = 'v', '<leader>lr', '<cmd>PhpTools Refactor<cr>' },
+    },
+    dependencies = {
+      -- "ccaglak/namespace.nvim", -- optional - php namespace resolver
+      -- "ccaglak/larago.nvim", -- optional -- laravel goto blade/components
+      -- "ccaglak/snippets.nvim", -- optional -- native snippet expander
+    },
+    config = function()
+      require('phptools').setup {
+        ui = {
+          enable = true, -- default:true; false only if you have a UI enhancement plugin
+          fzf = false, -- default:false; tests requires fzf used only in tests module otherwise there might long list  of tests
+        },
+        custom_toggles = { -- delete if you dont use it
+          enable = false, -- default:false
+          -- { "foo", "bar", "baz" }, -- Add more custom toggle groups here
+        },
+      }
+
+      local map = vim.keymap.set
+
+      local ide_helper = require 'phptools.ide_helper' -- delete if you dont use it
+      -- Laravel IDE Helper keymaps
+      map('n', '<leader>lha', ide_helper.generate_all, { desc = 'Generate all IDE helpers' })
+      map('n', '<leader>lhm', ide_helper.generate_models, { desc = 'Generate model helpers' })
+      map('n', '<leader>lhf', ide_helper.generate_facades, { desc = 'Generate facade helpers' })
+      map('n', '<leader>lht', ide_helper.generate_meta, { desc = 'Generate meta helper' })
+      map('n', '<leader>lhi', ide_helper.install, { desc = 'Install IDE Helper package' })
+
+      local tests = require 'phptools.tests' -- delete if you have a test plugin
+      map('n', '<Leader>ta', tests.test.all, { desc = 'Run all tests' })
+      map('n', '<Leader>tf', tests.test.file, { desc = 'Run current file tests' })
+      map('n', '<Leader>tl', tests.test.line, { desc = 'Run test at cursor' })
+      map('n', '<Leader>ts', tests.test.filter, { desc = 'Search and run test' })
+      map('n', '<Leader>tp', tests.test.parallel, { desc = 'Run tests in parallel' })
+      map('n', '<Leader>tr', tests.test.rerun, { desc = 'Rerun last test' })
+      map('n', '<Leader>ti', tests.test.selected, { desc = 'Run selected test file' })
+    end,
+  },
   {
     -- Main LSP Configuration
     'neovim/nvim-lspconfig',
@@ -1032,6 +1082,32 @@ require('lazy').setup({
     },
   },
 })
+
+-- Vimfony para Symfony dev
+local lspconfig = require 'lspconfig'
+local configs = require 'lspconfig.configs'
+local util = lspconfig.util
+
+configs.vimfony = {
+  default_config = {
+    cmd = { 'vimfony' },
+    filetypes = { 'php', 'twig', 'yaml', 'xml' }, -- You can remove file types if you don't like it, but then it won't work in those files
+    root_dir = function(fname)
+      return util.root_pattern('composer.json', '.git')(fname)
+    end,
+    single_file_support = true,
+    init_options = {
+      roots = { 'templates' },
+      container_xml_path = util.root_pattern('composer.json', '.git')()
+        and util.root_pattern('composer.json', '.git')() .. '/var/cache/dev/App_KernelDevDebugContainer.xml', -- Where your container XML is
+      vendor_dir = util.root_pattern('composer.json', '.git')() and util.root_pattern('composer.json', '.git')() .. '/vendor', -- Where your vendor directory is
+      -- Optional:
+      -- php_path = "/usr/bin/php",
+    },
+  },
+}
+
+lspconfig.vimfony.setup {}
 
 -- The line beneath this is called `modeline`. See `:help modeline`
 -- vim: ts=2 sts=2 sw=2 et
